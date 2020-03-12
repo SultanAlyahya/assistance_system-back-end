7b0458e294c1e13b540983b55725a21786b6ec02
SultanAlyahya <38963523+SultanAlyahya@users.noreply.github.com>
2020-03-12 12:58:55 +0300

    OCR

--- src/routers/blindUserRouter.js
+++ src/routers/blindUserRouter.js
@@ -116,7 +116,7 @@ const form = multer({
 
 router.post('/User/image', userAuthorization, form.single('form'), async(req, res)=>{
     try{
-     const image = await sharp(req.file.buffer).resize({width:50, height:50}).jpeg().toBuffer()
+     const image = await sharp(req.file.buffer).resize({width:200, height:200}).jpeg().toBuffer()
      //console.log(image.toString('base64'))
      const text = await detectText(image, res)
      //console.log("befor sending: ",text)
