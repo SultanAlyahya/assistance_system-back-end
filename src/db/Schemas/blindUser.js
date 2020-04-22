0c2b7a0e0063548e2f655a8c54aa8e6fbaf342f8
SultanAlyahya <38963523+SultanAlyahya@users.noreply.github.com>
2020-04-22 18:09:09 +0300

    add get statistics

--- src/db/Schemas/blindUser.js
+++ src/db/Schemas/blindUser.js
@@ -7,6 +7,8 @@ const blindUserSchema = mongoose.Schema({
     name:{
         type:String,
         required:true,
+        maxlength:20,
+        minlength:3
     },
     email:{
         type:String,
